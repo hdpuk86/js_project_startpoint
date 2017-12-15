a25a737d80c61eeec543c6f1b5fd5554c3e5585d
Findsay <fiona.e.lindsay@gmail.com>
2017-12-15 18:53:42 +0000

    Remove placeholder ul creation in planetview"

--- client/src/views/planetView.js
+++ client/src/views/planetView.js
@@ -1,9 +1,5 @@
 var PlanetView = function(planet) {
-  var ul = document.createElement('ul');
-  var li = document.createElement('li');
-  li.innerText = planet.name;
-  ul.appendChild(li);
-  return ul;
+
 };
 
 module.exports = PlanetView;
