1956fe0a52a0146edfe97d81dad1dd590ef8c4fb
Findsay <fiona.e.lindsay@gmail.com>
2017-12-15 18:54:32 +0000

    Created createCircle function in planet view

--- client/src/views/planetView.js
+++ client/src/views/planetView.js
@@ -1,3 +1,13 @@
+function createCircle(pixelSize, colour){
+  var div = document.createElement('div');
+  div.style.borderRadius = '50%';
+  div.style.width = pixelSize;
+  div.style.height = pixelSize;
+  div.style.backgroundColor = colour;
+  return div;
+};
+
+
 var PlanetView = function(planet) {
 
 };
