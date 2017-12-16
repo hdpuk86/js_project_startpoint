b714cdce64edb96d71be265bb7b8bc02e4dbbca9
hdpuk86 <hayley.d.prior@gmail.com>
2017-12-16 11:20:32 +0000

    moves hover listener to homeView

--- client/src/app.js
+++ client/src/app.js
@@ -5,21 +5,7 @@ var PlanetView = require('./views/planetView');
 
 var onLoad = function() {
   new Request('/planets', HomeView);
-  new Request('/planets', addHoverListener);
   popup = new Popup();
-};
-
-function addHoverListener(planets){
-  planets.forEach(function(planet){
-    var circle = document.getElementById(`circle_${planet.name}`);
-    var hoverDiv = document.getElementById(`hover_${planet.name}_div`);
-    circle.addEventListener('mouseover', function(){
-      hoverDiv.style.visibility = "visible";
-    });
-    circle.addEventListener('mouseleave', function(){
-      hoverDiv.style.visibility = 'hidden';
-    });
-  })
 };
 
 window.addEventListener('load', onLoad);
--- client/src/views/homeView.js
+++ client/src/views/homeView.js
@@ -1,6 +1,16 @@
 var PlanetView = require('./planetView');
 var HoverView = require('./hoverView');
 
+function addHoverListener(planet){
+  var circle = document.getElementById(`circle_${planet.name}`);
+  var hoverDiv = document.getElementById(`hover_${planet.name}_div`);
+  circle.addEventListener('mouseover', function(){
+    hoverDiv.style.visibility = "visible";
+  });
+  circle.addEventListener('mouseleave', function(){
+    hoverDiv.style.visibility = 'hidden';
+  });
+};
 
 var HomeView = function(planets) {
   var div = document.getElementById('planets-div');
@@ -9,6 +19,7 @@ var HomeView = function(planets) {
     var hoverDiv = new HoverView(planet);
     planetDiv.appendChild(hoverDiv);
     div.appendChild(planetDiv);
+    addHoverListener(planet);
   });
 };
 
