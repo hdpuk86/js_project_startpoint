d9cc4487c095eae1e70f4077c7ece7a2718a2346
hdpuk86 <hayley.d.prior@gmail.com>
2017-12-16 09:40:11 +0000

    adds mass and volume to seeds

--- client/src/views/hoverView.js
+++ client/src/views/hoverView.js
@@ -11,16 +11,15 @@ function createHoverDiv(planet){
 function createPlanetStats(planet){
   var ul = document.createElement('ul');
   var day = document.createElement('li');
-  day.innerText = "Day length: " + planet.dayLength;
+  day.innerText = "Day length: " + planet.dayLength + " (hours)";
   var year = document.createElement('li');
-  year.innerText = "Year length: " + planet.yearLength;
-  // -------Need to add mass and volumes to seeds ------
-  // var mass = document.createElement('li');
-  // mass.innerText = "Mass: " + planet.mass;
-  // var volume = document.createElement('li');
-  // volume.innerText = "Volume: " + planet.volume;
-  // ul.appendChild(volume);
-  // ul.appendChild(mass);
+  year.innerText = "Year length: " + planet.yearLength + " (Earth days)";
+  var mass = document.createElement('li');
+  mass.innerText = "Mass: " + planet.mass  + " (Earths)";
+  var volume = document.createElement('li');
+  volume.innerText = "Volume: " + planet.volume + " (Earths)";
+  ul.appendChild(volume);
+  ul.appendChild(mass);
   ul.appendChild(day);
   ul.appendChild(year);
   return ul;
