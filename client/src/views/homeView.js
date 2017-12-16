364014ea8aefcddf18082bc20ca1dfaa1c1dbfcf
hdpuk86 <hayley.d.prior@gmail.com>
2017-12-16 10:14:42 +0000

    fixes listener error and adds hover glow

--- client/src/views/homeView.js
+++ client/src/views/homeView.js
@@ -1,16 +1,6 @@
 var PlanetView = require('./planetView');
 var HoverView = require('./hoverView');
 
-function addHoverListener(planet){
-  var circle = document.getElementById(`circle_${planet.name}`);
-  var hoverDiv = document.getElementById(`hover_${planet.name}_div`);
-  circle.addEventListener('mouseover', function(){
-    hoverDiv.style.visibility = "visible";
-  });
-  circle.addEventListener('mouseover', function(){
-    hoverDiv.style.visibility = 'hidden';
-  });
-};
 
 var HomeView = function(planets) {
   var div = document.getElementById('planets-div');
@@ -19,7 +9,6 @@ var HomeView = function(planets) {
     var hoverDiv = new HoverView(planet);
     planetDiv.appendChild(hoverDiv);
     div.appendChild(planetDiv);
-    addHoverListener(planet);
   });
 };
 
