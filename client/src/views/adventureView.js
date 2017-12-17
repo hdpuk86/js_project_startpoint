91dd59ba76d945c69515a3d8c656b226b9f521f0
hdpuk86 <hayley.d.prior@gmail.com>
2017-12-17 13:19:00 +0000

    adds listeners to adventure

--- client/src/views/adventureView.js
+++ client/src/views/adventureView.js
@@ -1,10 +1,32 @@
 var AdventurePlanet = require('./adventurePlanetView');
+var Popup = require('./popupView');
+
+function addListeners(planet){
+  var circle = document.getElementById(`adventure_${planet.name}_circle`);
+  var hoverDiv = document.getElementById(`hover_${planet.name}_adventure`);
+  var nameDiv = document.getElementById(`${planet.name}_name_adventure`);
+  circle.addEventListener('mouseover', function(){
+    hoverDiv.style.visibility = "visible";
+    nameDiv.style.visibility = "visible";
+  });
+  circle.addEventListener('mouseleave', function(){
+    hoverDiv.style.visibility = 'hidden';
+    nameDiv.style.visibility = 'hidden';
+  });
+  circle.addEventListener('click', () => {
+    var popup = new Popup();
+    var newContent = planet.youtubeEmbed + planet.description;
+    popup.setContent(newContent);
+    popup.display();
+  });
+};
 
 var AdventureView = function(planets) {
   var div = document.getElementById('planet_container');
   planets.forEach(function(planet){
     var planetDiv = new AdventurePlanet(planet);
     div.appendChild(planetDiv);
+    addListeners(planet);
   });
 };
 
