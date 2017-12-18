be7efcdfd68b17b0d9ac33f3206b30862f0c5e37
hdpuk86 <hayley.d.prior@gmail.com>
2017-12-18 22:06:56 +0000

    adds hover listeners to name divs

--- client/src/views/adventureView.js
+++ client/src/views/adventureView.js
@@ -1,26 +1,36 @@
 var AdventurePlanet = require('./adventurePlanetView');
 var Popup = require('./popupView');
 
-function addListeners(planet){
+function addAllListeners(planet){
   var circle = document.getElementById(`adventure_${planet.name}_circle`);
-  var hoverDiv = document.getElementById(`hover_${planet.name}_adventure`);
   var nameDiv = document.getElementById(`${planet.name}_name_adventure`);
   var planetBtn = document.getElementById(`${planet.name}_button`);
-  circle.addEventListener('mouseover', function(){
+
+  addHoverListener(planet, circle);
+  addHoverListener(planet, nameDiv);
+
+  planetBtn.addEventListener('click', function(){
+    smoothScroll(planet.distance+2500);
+  })
+};
+
+function addHoverListener(planet, div){
+  var hoverDiv = document.getElementById(`hover_${planet.name}_adventure`);
+  div.addEventListener('mouseover', function(){
     hoverDiv.style.visibility = "visible";
   });
-  circle.addEventListener('mouseleave', function(){
+  div.addEventListener('mouseleave', function(){
     hoverDiv.style.visibility = 'hidden';
   });
-  circle.addEventListener('click', () => {
+};
+
+function addClickListener(planet, div){
+  div.addEventListener('click', () => {
     var popup = new Popup();
     var newContent = planet.youtubeEmbed + planet.description;
     popup.setContent(newContent);
     popup.display();
   });
-  planetBtn.addEventListener('click', function(){
-    smoothScroll(planet.distance+2500);
-  })
 };
 
 function smoothScroll(scrollToPx){
@@ -36,7 +46,7 @@ var AdventureView = function(planets) {
   planets.forEach(function(planet){
     var planetDiv = new AdventurePlanet(planet);
     div.appendChild(planetDiv);
-    addListeners(planet);
+    addAllListeners(planet);
   });
 };
 
