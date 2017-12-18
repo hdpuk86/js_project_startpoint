7058eca52d64d4f33037d5e570f90fbdcb10b29f
Jamie Klein <jamiesklein@gmail.com>
2017-12-18 11:05:36 +0000

    Puts a wobbly rocket man in the middle of the screen to appear along with the distance meter and navbar.

--- client/src/app.js
+++ client/src/app.js
@@ -6,25 +6,36 @@ var PlanetView = require('./views/planetView');
 var distanceMeter;
 var navbar;
 var rulerStart;
+var rocketMan;
+
+var shakeRocketMan = function() {
+  var currentTop = parseInt(window.getComputedStyle(rocketMan).getPropertyValue('top'));
+  var randomShift = Math.floor(Math.random() * 11 - 5);
+  rocketMan.style.top = (currentTop + randomShift) + 'px';
+};
 
 var onScroll = function() {
   var scrolled = window.scrollX;
   if (scrolled >= rulerStart) {
+    shakeRocketMan();
     var km = (scrolled - rulerStart) * 3474.2;
     var lightMinutes = km / 17987547.5;
     var rounded = Math.round(lightMinutes * 100) / 100; // to 2 d.p.
     distanceMeter.innerText = rounded + ' light minutes';
     navbar.style.visibility = 'visible';
     distanceMeter.style.visibility = 'visible';
+    rocketMan.style.visibility = 'visible';
   } else {
     navbar.style.visibility = 'hidden';
     distanceMeter.style.visibility = 'hidden';
+    rocketMan.style.visibility = 'hidden';
   }
 };
 
 var onLoad = function() {
   navbar = document.getElementById('space-nav');
   distanceMeter = document.getElementById('distance-meter');
+  rocketMan = document.getElementById('rocket_man');
   rulerStart = document.getElementById('planet_container').getBoundingClientRect().left - window.innerWidth/2;
   new Request('/planets', HomeView);
   new Request('/planets', AdventureView);
