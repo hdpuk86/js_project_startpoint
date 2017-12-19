f9ec01b6ce9ba4f559b61f0e7020deaf5309c8b2
Findsay <fiona.e.lindsay@gmail.com>
2017-12-19 12:07:00 +0000

    Changed background colour, and set max height of bar chart

--- client/src/models/barChart.js
+++ client/src/models/barChart.js
@@ -3,14 +3,26 @@ var Highcharts = require('highcharts');
 var BarChart = function(container, title, categories, series){
   var chart = new Highcharts.Chart({
     chart: {
+      backgroundColor: "black",
       type: "column",
       renderTo: container
     },
     title: {
-      text: title
+      text: title,
+      style: {
+        "fontFamily": "Wire One",
+        "color": "#FFFFFF",
+        "fontSize": "3em"
+      }
     },
     xAxis: {
       categories: categories
+    },
+    yAxis: {
+      max: 5
+    },
+    legend: {
+      enabled: false
     },
     series: series
   });
