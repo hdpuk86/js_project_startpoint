f03701f34a0b47684b562fec3e9e1ad84ebb13f7
hdpuk86 <hayley.d.prior@gmail.com>
2017-12-19 09:59:26 +0000

    changes popup alignment

--- client/src/views/homeView.js
+++ client/src/views/homeView.js
@@ -3,6 +3,7 @@ var Popup = require('./popupView');
 var Quiz = require('./quizView');
 var PieCompView = require('./pieCompView');
 var questionNumber;
+var NameView = require('./nameView');
 
 function addListeners(planet){
   var circle = document.getElementById(`circle_${planet.name}`);
@@ -10,14 +11,14 @@ function addListeners(planet){
   var nameDiv = document.getElementById(`${planet.name}_name_div`);
   var imgButton = createButton(planet);
   var pieChart = new PieCompView(planet);
+  var name = new NameView(planet);
+  name.className = "popup-title";
   hoverDiv.style.opacity = 0;
   hoverDiv.style.transition = 'opacity 1.0s linear';
   circle.addEventListener('mouseover', function(){
     hoverDiv.style.opacity = 1;
     hoverDiv.style.visibility = "visible";
     nameDiv.style.visibility = "visible";
-
-
   });
   circle.addEventListener('mouseleave', function(){
     hoverDiv.style.visibility = 'hidden';
@@ -27,14 +28,29 @@ function addListeners(planet){
   circle.addEventListener('click', () => {
     popup = new Popup();
     var div = document.createElement('div');
+    var upperSection = document.createElement('section');
+    upperSection.className = "popup-upper";
     var videoDiv = document.createElement('div');
-    var textDiv = document.createElement('div');
     videoDiv.innerHTML = planet.youtubeEmbed;
+    upperSection.appendChild(name);
+    upperSection.appendChild(videoDiv);
+
+    var lowerSection = document.createElement('section');
+    lowerSection.className = "popup-lower";
+    var textDiv = document.createElement('div');
+    textDiv.className = "popup-text";
     textDiv.innerHTML = planet.description;
-    div.appendChild(videoDiv);
-    div.appendChild(textDiv);
-    div.appendChild(pieChart);
-    div.appendChild(imgButton);
+    lowerSection.appendChild(textDiv);
+    lowerSection.appendChild(pieChart);
+
+    var nav = document.createElement('nav');
+    nav.className = "popup-nav";
+    nav.appendChild(imgButton);
+
+    div.appendChild(upperSection);
+    div.appendChild(lowerSection);
+    div.appendChild(nav);
+
     popup.setContent(div);
     popup.display();
   });
