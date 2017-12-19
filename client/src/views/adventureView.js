89d210fece27d99daa77e692d17b3ce24dfdc733
hdpuk86 <hayley.d.prior@gmail.com>
2017-12-19 10:03:14 +0000

    re-aligns popup view in adventure

--- client/src/views/adventureView.js
+++ client/src/views/adventureView.js
@@ -4,6 +4,7 @@ var Popup = require('./popupView');
 var Quiz = require('./quizView');
 var PieCompView = require('./pieCompView');
 var questionNumber;
+var NameView = require('./nameView');
 
 function addAllListeners(planet){
   var circle = document.getElementById(`adventure_${planet.name}_circle`);
@@ -33,17 +34,34 @@ function addHoverListener(planet, div){
 function addClickListener(planet, div){
   var imgButton = createButton(planet);
   var pieChart = new PieCompView(planet);
+  var name = new NameView(planet);
+  name.className = "popup-title";
   div.addEventListener('click', () => {
     popup = new Popup();
     var div = document.createElement('div');
+    var upperSection = document.createElement('section');
+    upperSection.className = "popup-upper";
     var videoDiv = document.createElement('div');
-    var textDiv = document.createElement('div');
     videoDiv.innerHTML = planet.youtubeEmbed;
+    upperSection.appendChild(name);
+    upperSection.appendChild(videoDiv);
+
+    var lowerSection = document.createElement('section');
+    lowerSection.className = "popup-lower";
+    var textDiv = document.createElement('div');
+    textDiv.className = "popup-text";
     textDiv.innerHTML = planet.description;
-    div.appendChild(videoDiv);
-    div.appendChild(textDiv);
-    div.appendChild(pieChart);
-    div.appendChild(imgButton);
+    lowerSection.appendChild(textDiv);
+    lowerSection.appendChild(pieChart);
+
+    var nav = document.createElement('nav');
+    nav.className = "popup-nav";
+    nav.appendChild(imgButton);
+
+    div.appendChild(upperSection);
+    div.appendChild(lowerSection);
+    div.appendChild(nav);
+
     popup.setContent(div);
     popup.display();
   });
